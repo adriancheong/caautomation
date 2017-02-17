651ccd6f539cabd528a2f47318f7e77d29175b66
acheongy <acheongy@VS-CLOUD-D0671>
2017-02-17 17:51:43 +0800

    changes

--- src/CAAutomation/Controllers/HomeController.cs
+++ src/CAAutomation/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace CAAutomation.Controllers
 {
@@ -30,6 +32,28 @@ namespace CAAutomation.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+        [HttpPost("Notify")]
+        public void Notify()
+        {
+            Notify("+6597738072", "+601117223073", "From C#!");
+        }
+
+        private static void Notify(string to, string from, string body)
+        {
+            string AccountSid = "AC745b14f8063bd0bbdc6e14bfd223a166";
+            string AuthToken = "31d1c25104176b3fdade0b035e589c85";
+
+            var client = new RestClient("https://api.twilio.com/2010-04-01/Accounts/AC745b14f8063bd0bbdc6e14bfd223a166/Messages");
+            client.Authenticator = new HttpBasicAuthenticator(AccountSid, AuthToken);
+            var request = new RestRequest(Method.POST);
+
+            request.AddParameter("To", to);
+            request.AddParameter("From", from);
+            request.AddParameter("Body", body);
+
+            var response = client.Execute(request);
         }
     }
 }
